9f35206d0d391de7cf0cbf48a7708a0ee6bed728
DanielBrisch <124108211+DanielBrisch@users.noreply.github.com>
2023-02-16 18:42:22 -0300

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World!")
+    val titular: String = "Daniel"
+    val numConta: Int = 100
+    var saldo = 0.0
+
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular $titular")
+    println("numero da conta $numConta")
+    println("Saldo da conta $saldo")
+}
