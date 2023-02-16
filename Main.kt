7ef590e815310476c25b32e23b74356c4ee11b79
DanielBrisch <124108211+DanielBrisch@users.noreply.github.com>
2023-02-16 20:05:41 -0300

    commit-1

--- Main.kt
+++ Main.kt
@@ -10,4 +10,19 @@ fun main() {
     println("titular $titular")
     println("numero da conta $numConta")
     println("Saldo da conta $saldo")
-}
+
+    if (saldo >= 0.0) {
+        println("Conta com saldo positivo ou igual a zero")
+    } else {
+        println("Conta NEGATIVA")
+    }
+
+
+    when {
+        saldo > 0.0 -> println("conta é Positiva")
+        saldo == 0.0 -> println("Conta Zerada")
+        else -> println("Conta Negativa")
+    }
+
+    
+    }
